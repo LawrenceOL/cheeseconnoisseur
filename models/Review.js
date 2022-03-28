5eae3c9945847ac367daf487b0d32c928b9f6af9
LawrenceOL <loreganlloyd@gmail.com>
2022-03-28 12:34:16 -0400

    Revised Review schema and Cheese seed

--- models/Review.js
+++ models/Review.js
@@ -4,7 +4,6 @@ const Review = new Schema(
   {
     reviewer_name: { type: String, required: false },
     details: { type: String, required: false },
-    rating: { type: Schema.Types.ObjectId, ref: 'Rating' },
     cheese: { type: Schema.Types.ObjectId, ref: 'Cheese' }
   },
   { timestamps: true }
--- seed/Cheese.js
+++ seed/Cheese.js
@@ -25,6 +25,16 @@ const main = async () => {
       cheesemaker: null,
       Review: null,
       rating: null
+    },
+    {
+      name: "Brewster's Own Mozzarella",
+      description:
+        "Mozzarella is a southern Italian cheese traditionally made from Italian buffalo's milk by the pasta filata method. Fresh mozzarella is generally white but when seasoned it turns to a light yellow depending on the animal's diet.",
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Burrata2.jpg/220px-Burrata2.jpg',
+      cheesemaker: null,
+      Review: null,
+      rating: null
     }
   ]
 
