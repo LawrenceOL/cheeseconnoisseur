0e0ef55c8103f9595562e58fdbf13dbc830f920c
LawrenceOL <loreganlloyd@gmail.com>
2022-03-29 14:46:04 -0400

    added display of cheeses on home page

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
-import { StrictMode } from "react";
-import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -2,8 +2,11 @@ const About = () => {
   return (
     <div>
       <h1>About</h1>
+      <p>
+        Welcome to the wide world of cheese! This page was created by LawrenceOL
+      </p>
     </div>
-  );
-};
+  )
+}
 
-export default About;
+export default About
