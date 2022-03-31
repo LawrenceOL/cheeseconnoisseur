af026a1d42a5291f7f5c14bd7b6faae92cfa3b28
LawrenceOL <loreganlloyd@gmail.com>
2022-03-31 14:37:45 -0400

    added new cheesemaker confirmation page

---
+++ client/src/pages/ConfirmCheesemaker.js
@@ -0,0 +1,9 @@
+const ConfirmCheeseMaker = () => {
+  return (
+    <div>
+      <p>Congratulations! You added a new cheesemaker to the page.</p>
+    </div>
+  )
+}
+
+export default ConfirmCheeseMaker
