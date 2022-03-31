ffa04f2af57460ac9ca09583f8c2978cdf54ec34
LawrenceOL <loreganlloyd@gmail.com>
2022-03-31 19:26:04 -0400

    removed package.json

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const db = require('./db')
 const logger = require('morgan')
 const cors = require('cors')
 
-//middleware
+//middlewares
 
 const PORT = process.env.PORT || 3001
 const app = express()
