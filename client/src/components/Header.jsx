8068d81906cc9dee2dbb7d8371b7f0cf1592fe2f
Lawrence O'Regan-Lloyd <loreganlloyd@gmail.com>
2022-07-30 13:40:41 -0400

    updated header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,16 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">Cheeses</Link>
         <Link to="/cheesemakers">Cheese Makers</Link>
         <Link to="/about">About</Link>
         <Link to="/addcheeseform">Add a Cheese</Link>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
